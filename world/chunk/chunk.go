13300877b838fd1622c32d873bceac577ff88e8b
Code Hz <CodeHz@outlook.com>
2019-09-09 20:50:54 +0800

    add chunk

---
+++ world/chunk/chunk.go
@@ -0,0 +1,130 @@
+package chunk
+
+import (
+	"errors"
+	"sort"
+
+	packed "github.com/EndCrystal/PackedIO"
+	"github.com/EndCrystal/Server/world/block"
+)
+
+type BlockPos uint16
+
+func Pos(x, y, z uint8) BlockPos {
+	return BlockPos(x) + 16*(BlockPos(z)+16*BlockPos(y))
+}
+
+func (p BlockPos) X() uint8 { return uint8(p) % 16 }
+func (p BlockPos) Z() uint8 { return uint8(p/16) % 16 }
+func (p BlockPos) Y() uint8 { return uint8(p / 256) }
+
+// Format:
+// <Map local id to block.Instance>
+// (length: varuint32) * { (block name: string) [aux: varuint32] [color: uint32] }
+// <Block data (constant 65536)>
+// 65536 * (local id: varuint32)
+// <Background Block data (constant 65536)>
+// 65536 * (local id: varuint32)
+// <Extra data>
+// (length: varuint32) * { (position: uint16) (extradata: data) }
+type Chunk struct {
+	Blocks     [^BlockPos(0)]block.Instance
+	Background [^BlockPos(0)]block.Instance
+	ExtraData  map[BlockPos]packed.Serializable
+}
+
+var ECorruptedData = errors.New("Failed to load chunk data: corrupted data")
+var EUnknownBlock = errors.New("Failed to load chunk data: unknown block")
+
+func (chk *Chunk) Load(in packed.Input) {
+	var maps []block.Instance
+	in.IterateArray(func(length int) { maps = make([]block.Instance, length) }, func(i int) {
+		name := in.ReadString()
+		blk, ok := block.LookupId(name)
+		if !ok {
+			panic(ECorruptedData)
+		}
+		maps[i].Id = blk
+		b := block.Get(blk)
+		if b == nil {
+			panic(EUnknownBlock)
+		}
+		if b.HasAux() {
+			maps[i].Aux = in.ReadVarUint32()
+		}
+		if b.HasColor() {
+			maps[i].Color = in.ReadUint32()
+		}
+	})
+	lmaps := uint32(len(maps))
+	for i := BlockPos(0); i < ^BlockPos(0); i++ {
+		idx := in.ReadVarUint32()
+		if idx >= lmaps {
+			panic(ECorruptedData)
+		}
+		chk.Blocks[i] = maps[idx]
+	}
+	for i := BlockPos(0); i < ^BlockPos(0); i++ {
+		idx := in.ReadVarUint32()
+		if idx >= lmaps {
+			panic(ECorruptedData)
+		}
+		chk.Background[i] = maps[idx]
+	}
+	in.IterateArray(nil, func(int) {
+		pos := BlockPos(in.ReadUint16())
+		id := chk.Blocks[pos].Id
+		loader := block.GetExtraLoader(id)
+		if loader == nil {
+			panic(ECorruptedData)
+		}
+		chk.ExtraData[pos] = loader(in)
+	})
+}
+
+func (chk Chunk) Save(out packed.Output) {
+	maps := make(map[block.Instance]int)
+	for _, ins := range chk.Blocks {
+		maps[ins]++
+	}
+	for _, ins := range chk.Background {
+		maps[ins]++
+	}
+	num := len(maps)
+	list := make([]struct {
+		ins   block.Instance
+		count int
+	}, num)
+	i := 0
+	for ins, count := range maps {
+		list[i].ins = ins
+		list[i].count = count
+	}
+	sort.Slice(list, func(a int, b int) bool { return list[i].count < list[i].count })
+	rmap := make(map[block.Instance]int)
+	out.WriteVarUint32(uint32(num))
+	for i, tpl := range list {
+		ins := tpl.ins
+		blk := ins.GetBlock()
+		out.WriteString(blk.Name)
+		if blk.HasAux() {
+			out.WriteVarUint32(ins.Aux)
+		}
+		if blk.HasColor() {
+			out.WriteUint32(ins.Color)
+		}
+		rmap[ins] = i
+	}
+	for _, ins := range chk.Blocks {
+		idx := rmap[ins]
+		out.WriteVarUint32(uint32(idx))
+	}
+	for _, ins := range chk.Background {
+		idx := rmap[ins]
+		out.WriteVarUint32(uint32(idx))
+	}
+	out.WriteVarUint32(uint32(len(chk.ExtraData)))
+	for _, data := range chk.ExtraData {
+		data.Save(out)
+	}
+}
