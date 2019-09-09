2f81ce491d9594af3f0e860858a8b152e7a7244e
Code Hz <CodeHz@outlook.com>
2019-09-09 22:17:45 +0800

    fix loginpacket quota

--- packet/login.go
+++ packet/login.go
@@ -72,4 +72,6 @@ func (pkt LoginPacket) Read() (payload LoginPayload, ok bool) {
 
 func (pkt LoginPacket) PacketId() PacketId { return IdLogin }
 
-func (pkt LoginPacket) Check(pctx *ParseContext) bool { return pctx.Check(ClientSide, 1024) }
+func (pkt LoginPacket) Check(pctx *ParseContext) bool {
+	return pctx.Check(ClientSide, 1024+uint16(len(pkt.raw)))
+}
