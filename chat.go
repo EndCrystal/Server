61929ebde692bb3fd81baffdbdaa49760e918c5e
CodeHz <CodeHz@Outlook.com>
2019-09-22 22:13:06 +0800

    add chat log

--- chat.go
+++ chat.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"github.com/EndCrystal/Server/logprefix"
 	"github.com/EndCrystal/Server/network"
 	"github.com/EndCrystal/Server/packet"
 )
 
 func handleChat(broadcaster network.PacketBroadcaster) chan<- ChatMessage {
+	log := logprefix.Get("[chat system] ")
 	ret := make(chan ChatMessage)
 	go func() {
 		for msg := range ret {
+			log.Printf("<%s> %s", msg.Sender, msg.Message)
 			broadcaster.BroadcastPacket(&packet.TextPacket{
 				Flags:   packet.TextPacketNormal,
 				Sender:  msg.Sender,
