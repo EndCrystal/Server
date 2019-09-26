f7d15448e2a2a165ecbca0dee26343c0fafc303a
CodeHz <CodeHz@Outlook.com>
2019-09-27 07:59:38 +0800

    add storage layer

--- go.mod
+++ go.mod
@@ -6,5 +6,7 @@ require (
 	github.com/EndCrystal/PackedIO v1.0.4
 	github.com/julienschmidt/httprouter v1.2.0
 	github.com/rs/cors v1.7.0
+	go.etcd.io/bbolt v1.3.3
+	golang.org/x/sys v0.0.0-20190926180325-855e68c8590b // indirect
 	nhooyr.io/websocket v1.5.1
 )
---
+++ world/storage/storage.go
@@ -0,0 +1,70 @@
+package storage
+
+import (
+	"bytes"
+	"fmt"
+	"time"
+
+	packed "github.com/EndCrystal/PackedIO"
+	"github.com/EndCrystal/Server/world/chunk"
+	"go.etcd.io/bbolt"
+)
+
+type Storage struct{ db *bbolt.DB }
+
+func Open(path string) (ret *Storage, err error) {
+	db, err := bbolt.Open(path, 0600, &bbolt.Options{Timeout: 1 * time.Second})
+	if err == nil {
+		ret = &Storage{db}
+	}
+	return
+}
+
+func (s *Storage) Close() {
+	s.db.Close()
+}
+
+func (s *Storage) ForDim(name string) StorageForDim {
+	return StorageForDim{s, name}
+}
+
+type StorageForDim struct {
+	*Storage
+	dim string
+}
+
+func dump(obj packed.Serializable) []byte {
+	o, buf := packed.NewOutput()
+	obj.Save(o)
+	return buf.Bytes()
+}
+
+func (s StorageForDim) LoadChunk(pos chunk.ChunkPos) (chk *chunk.Chunk, err error) {
+	dimchk := []byte(fmt.Sprintf("dim-%s-chunks", s.dim))
+	err = s.db.View(func(tx *bbolt.Tx) error {
+		bkt := tx.Bucket(dimchk)
+		if bkt == nil {
+			return chunk.EChunkNotFound
+		}
+		data := bkt.Get(dump(&pos))
+		var chkdata chunk.Chunk
+		chkdata.Load(packed.MakeInput(bytes.NewReader(data)))
+		chk = &chkdata
+		return nil
+	})
+	return
+}
+
+func (s StorageForDim) SaveChunk(pos chunk.ChunkPos, data *chunk.Chunk) error {
+	dimchk := []byte(fmt.Sprintf("dim-%s-chunks", s.dim))
+	posd := dump(&pos)
+	datad := dump(data)
+	err := s.db.Batch(func(tx *bbolt.Tx) error {
+		bkt, e := tx.CreateBucketIfNotExists(dimchk)
+		if e != nil {
+			return e
+		}
+		return bkt.Put(posd, datad)
+	})
+	return err
+}
