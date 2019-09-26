4908fa953f1fdf4b875b4674dc8a8c0517c56244
CodeHz <CodeHz@Outlook.com>
2019-09-26 22:49:16 +0800

    fix Identifier management

--- world/actor/actor.go
+++ world/actor/actor.go
@@ -13,17 +13,26 @@ type (
 )
 
 var (
-	maxId Id
-	mtx   sync.Mutex
+	maxId    Id
+	freelist map[Id]struct{}
+	mtx      sync.Mutex
 )
 
 func (id Identifier) ID() Id { return id.Id }
 
 func AllocIdentifier() Id {
 	mtx.Lock()
-	defer func() {
-		maxId++
-		mtx.Unlock()
-	}()
+	defer mtx.Unlock()
+	for id := range freelist {
+		defer delete(freelist, id)
+		return id
+	}
+	defer func() { maxId++ }()
 	return maxId
+}
+
+func DeleteIdentifier(id Id) {
+	mtx.Lock()
+	defer mtx.Unlock()
+	freelist[id] = struct{}{}
 }
---
+++ world/player/player.go
@@ -0,0 +1,7 @@
+package player
+
+import "github.com/EndCrystal/Server/world/actor"
+
+type Player struct {
+	actor.Identifier
+}
